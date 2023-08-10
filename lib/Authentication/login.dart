1352102f076baf6c6f06c8ffe2a4b3c8cfd0e5cb
Lxghtning <101966584+Lxghtning@users.noreply.github.com>
2023-08-10 19:41:32 +0530

    2nd

--- lib/Authentication/login.dart
+++ lib/Authentication/login.dart
@@ -12,7 +12,6 @@ class Login extends StatefulWidget{
 class _LoginState extends State<Login>{
 
   final AuthService _auth = AuthService();
-  // final Database _firestoreDatabase = Database();
 
   String email = '';
   String password = '';
@@ -21,22 +20,16 @@ class _LoginState extends State<Login>{
 
   bool passState = true;
 
-  var items = [
-    'Male',
-    'Female',
-    'Other',
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
           child: Container(
-            child: const Column(
+            child: Column(
               children: [
-                Padding(
+                const Padding(
                     padding: EdgeInsets.only(
-                        left: 25,
+                        right: 100,
                         top: 150,
                     ),
                     child: Text(
@@ -46,8 +39,126 @@ class _LoginState extends State<Login>{
                         fontWeight: FontWeight.bold,
                       ),
                       textAlign: TextAlign.center,
-
                     ),
+                  ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(30, 100, 50, 0),
+                  child: TextFormField(
+                      decoration: const InputDecoration(
+                        labelText: "Email",
+                        hintText: "Enter your email",
+                      ),
+                      onChanged: (value){
+                        bool isValid = EmailValidator.validate(value);
+                        if(isValid) {
+                          setState(() {
+                            error = "";
+                            email = value;
+                          });
+                        }else{
+                          setState(() {
+                            error = "Invalid Email!";
+                          });
+                        }
+
+                      }
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(30, 20, 50, 0),
+                  child: TextFormField(
+                    obscureText: passState,
+                    decoration: InputDecoration(
+                      labelText: "Password",
+                      hintText: "Enter your password",
+                      suffixIcon: IconButton(
+                        icon: passState ? const Icon(Icons.visibility) : const Icon(Icons.visibility_off),
+                        onPressed: (){
+                          setState(() {
+                            passState = !passState;
+                          });
+                        },
+                      ),
+                    ),
+                    onChanged: (value){
+                      setState(() {
+                        password = value;
+                      });
+                    },
+                  ),
+                ),
+                Text(
+                    error,
+                    style: const TextStyle(
+                      color: Colors.red,
+                    )
+                ),
+            Row(
+              children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(10, 10, 200, 10),
+                        child: InkWell(
+                        child: const Text(
+                          'Forgot Password?',
+                          style: TextStyle(
+                            color: Colors.blueAccent,
+                          ),
+                        ),
+                        onTap: () {
+                          showDialog(context: context, builder: (BuildContext context){
+                            return AlertDialog(
+                              title: const Text("Enter your email"),
+                              content: TextField(
+                                decoration: const InputDecoration(
+                                  labelText: "Email",
+                                  hintText: "Enter email",
+                                ),
+                                onChanged: (value){
+                                  setState(() {
+                                    resetEmail = value;
+                                  });
+                                },
+                              ),
+                              actions: <Widget>[
+                                ElevatedButton(
+                                  style: ButtonStyle(
+                                    backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
+                                  ),
+                                  child: const Text('Send',style:TextStyle(color: Colors.black)),
+                                  onPressed: () async{
+                                    await _auth.resetPassword(resetEmail);
+                                    Navigator.of(context).pop();
+                                  },
+                                ),
+                                ElevatedButton(
+                                  style: ButtonStyle(
+                                    backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
+                                  ),
+                                  child: const Text('Close', style: TextStyle(
+                                    color: Colors.black,
+                                  )),
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                ),
+                              ],
+                            );
+                          });
+                        },
+                    ),
+                      ),
+                      InkWell(
+                        child: const Text(
+                          'Do not have an account? Register',
+                          style: TextStyle(
+                            color: Colors.blueAccent,
+                          ),
+                        ),
+                        onTap: () {
+                          Navigator.pushReplacementNamed(context, '/register');
+                        },
+                      ),
+                    ],
                   ),
 
               ],
