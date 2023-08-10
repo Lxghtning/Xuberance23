d84739227bbdbb0d105c0640c8c4c8df7ad49b41
Lxghtning <101966584+Lxghtning@users.noreply.github.com>
2023-08-11 00:34:38 +0530

    4th

--- lib/Authentication/login.dart
+++ lib/Authentication/login.dart
@@ -24,149 +24,181 @@ class _LoginState extends State<Login>{
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-          child: Container(
-            child: Column(
-              children: [
-                const Padding(
-                    padding: EdgeInsets.only(
-                        right: 100,
-                        top: 150,
-                    ),
-                    child: Text(
-                      'Welcome Back',
-                      style: TextStyle(
-                        fontSize: 40,
-                        fontWeight: FontWeight.bold,
+          child: SingleChildScrollView(
+            child: SizedBox(
+              child: Column(
+                children: [
+                  const Padding(
+                      padding: EdgeInsets.only(right: 10, top: 150,),
+                      child: Text(
+                        'Welcome Back!',
+                        style: TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.center,
                       ),
-                      textAlign: TextAlign.center,
+                    ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(30, 100, 50, 0),
+                    child: TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: "Email",
+                          hintText: "Enter your email",
+                        ),
+                        onChanged: (value){
+                          bool isValid = EmailValidator.validate(value);
+                          if(isValid) {
+                            setState(() {
+                              error = "";
+                              email = value;
+                            });
+                          }else{
+                            setState(() {
+                              error = "Invalid Email!";
+                            });
+                          }
+
+                        }
                     ),
                   ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(30, 100, 50, 0),
-                  child: TextFormField(
-                      decoration: const InputDecoration(
-                        labelText: "Email",
-                        hintText: "Enter your email",
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(30, 20, 50, 0),
+                    child: TextFormField(
+                      obscureText: passState,
+                      decoration: InputDecoration(
+                        labelText: "Password",
+                        hintText: "Enter your password",
+                        suffixIcon: IconButton(
+                          icon: passState ? const Icon(Icons.visibility) : const Icon(Icons.visibility_off),
+                          onPressed: (){
+                            setState(() {
+                              passState = !passState;
+                            });
+                          },
+                        ),
                       ),
                       onChanged: (value){
-                        bool isValid = EmailValidator.validate(value);
-                        if(isValid) {
-                          setState(() {
-                            error = "";
-                            email = value;
-                          });
-                        }else{
-                          setState(() {
-                            error = "Invalid Email!";
-                          });
-                        }
-
-                      }
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(30, 20, 50, 0),
-                  child: TextFormField(
-                    obscureText: passState,
-                    decoration: InputDecoration(
-                      labelText: "Password",
-                      hintText: "Enter your password",
-                      suffixIcon: IconButton(
-                        icon: passState ? const Icon(Icons.visibility) : const Icon(Icons.visibility_off),
-                        onPressed: (){
-                          setState(() {
-                            passState = !passState;
-                          });
-                        },
-                      ),
+                        setState(() {
+                          password = value;
+                        });
+                      },
                     ),
-                    onChanged: (value){
-                      setState(() {
-                        password = value;
-                      });
-                    },
                   ),
-                ),
-                Text(
-                    error,
-                    style: const TextStyle(
-                      color: Colors.red,
-                    )
-                ),
-            Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(30, 0, 50, 0),
-                  child: InkWell(
-                    child: const Text(
-                      'Register',
-                      style: TextStyle(
-                        color: Colors.blueAccent,
-                      ),
-                    ),
-                    onTap: () {
-                      Navigator.pushReplacementNamed(context, '/register');
-                    },
+                  Text(
+                      error,
+                      style: const TextStyle(
+                        color: Colors.red,
+                      )
                   ),
-                ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(120, 10, 0, 10),
-                        child: InkWell(
-                        child: const Text(
-                          'Forgot Password?',
-                          style: TextStyle(
-                            color: Colors.blueAccent,
-                          ),
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(30, 0, 50, 0),
+                    child: InkWell(
+                      child: const Text(
+                        'Register',
+                        style: TextStyle(
+                          color: Colors.blueAccent,
                         ),
-                        onTap: () {
-                          showDialog(context: context, builder: (BuildContext context){
-                            return AlertDialog(
-                              title: const Text("Enter your email"),
-                              content: TextField(
-                                decoration: const InputDecoration(
-                                  labelText: "Email",
-                                  hintText: "Enter email",
-                                ),
-                                onChanged: (value){
-                                  setState(() {
-                                    resetEmail = value;
-                                  });
-                                },
-                              ),
-                              actions: <Widget>[
-                                ElevatedButton(
-                                  style: ButtonStyle(
-                                    backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
-                                  ),
-                                  child: const Text('Send',style:TextStyle(color: Colors.black)),
-                                  onPressed: () async{
-                                    await _auth.resetPassword(resetEmail);
-                                    Navigator.of(context).pop();
-                                  },
-                                ),
-                                ElevatedButton(
-                                  style: ButtonStyle(
-                                    backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
-                                  ),
-                                  child: const Text('Close', style: TextStyle(
-                                    color: Colors.black,
-                                  )),
-                                  onPressed: () {
-                                    Navigator.of(context).pop();
-                                  },
-                                ),
-                              ],
-                            );
-                          });
-                        },
-                    ),
                       ),
-
-                    ],
+                      onTap: () {
+                        Navigator.pushReplacementNamed(context, '/register');
+                      },
+                    ),
                   ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(120, 10, 0, 10),
+                          child: InkWell(
+                          child: const Text(
+                            'Forgot Password?',
+                            style: TextStyle(
+                              color: Colors.blueAccent,
+                            ),
+                          ),
+                          onTap: () {
+                            showDialog(context: context, builder: (BuildContext context){
+                              return AlertDialog(
+                                title: const Text("Enter your email"),
+                                content: TextField(
+                                  decoration: const InputDecoration(
+                                    labelText: "Email",
+                                    hintText: "Enter email",
+                                  ),
+                                  onChanged: (value){
+                                    setState(() {
+                                      resetEmail = value;
+                                    });
+                                  },
+                                ),
+                                actions: <Widget>[
+                                  ElevatedButton(
+                                    style: ButtonStyle(
+                                      backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
+                                    ),
+                                    child: const Text('Send',style:TextStyle(color: Colors.black)),
+                                    onPressed: () async{
+                                      await _auth.resetPassword(resetEmail);
+                                      Navigator.of(context).pop();
+                                    },
+                                  ),
+                                  ElevatedButton(
+                                    style: ButtonStyle(
+                                      backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
+                                    ),
+                                    child: const Text('Close', style: TextStyle(
+                                      color: Colors.black,
+                                    )),
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                  ),
+                                ],
+                              );
+                            });
+                          },
+                      ),
+                        ),
 
-              ],
-            )
+                      ],
+                    ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(30, 20, 50, 0),
+                    child: SizedBox(
+                      width: 400,
+                      height: 65,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.black, // Set the background color here
+                        ),
+                        onPressed: () async{
+                          try {
+                            await _auth
+                                .signInUserwithEmailAndPassword(email,
+                                password);
+
+                          }catch(e){
+                            if (e is FirebaseAuthException && e.code == 'user-not-found') {
+                              setState(() {
+                                error = "User not found! Try registering.";
+                              });
+                            }
+                          }
+                        },
+                        child: const Text(
+                            "Login",
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.white,
+                            )
+                        ),
+
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ),
           ),
         )
     );
